460e22a53b311809da0699080ec1e31d8ce92ac0
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-05-17 23:49:40 +0100

    (chore) finishishing opinion routes, and auth routes and spliting into routes files

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import mongoose from 'mongoose';
 import methodOverride from 'method-override';
+import flash from 'connect-flash';
 import Initiative from './models/initiative';
 import Opinion from './models/opinion';
+import User from './models/user';
+import Vote from './models/voters';
 import seedDB from './seeds';
+
+
+// ROUTES
+
+import initiativeRoutes from './routes/initiatives';
+import opinionRoutes from './routes/opinions';
+import authRoutes from './routes/auth';
+import voteRoutes from './routes/votes';
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -13,6 +27,7 @@ mongoose.connect('mongodb://localhost/andela_initiative_comments');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(methodOverride('_method'));
+app.use(flash());
 
 mongoose.Promise = require('bluebird');
 
@@ -20,115 +35,44 @@ app.set('view engine', 'ejs');
 
 seedDB();
 
-// Initiative.remove({}, (err, removed)=>{
-//   if(err) {
-//     console.log(err);
-//   }
-// });
-// Initiative.create(
-//   { title: 'Salmon Creek',
-//   image: ,
-//   description: 'Kielbasa cow bacon doner jowl ham hock tri-tip pork belly meatball. Jowl drumstick kevin prosciutto shank cupim shoulder. Pancetta turkey brisket pork loin alcatra. Jerky sausage '}, (err, opinion) =>{
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(opinion);
-//   }
-// });
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: "Anywhere I go",
+  resave: false,
+  saveUninitialized: false
+}));
 
-// INDEX ROUTE
-app.get('/andelainitiative', (req, res) => {
-  Initiative.find({}, (err, allInitiatives) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('index', { initiatives: allInitiatives });
-    }
-  });
-});
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-// SHOW ROUTE
-app.get('/andelainitiative/:id', (req, res) => {
-  Initiative.findById(req.params.id).populate('opinions').exec((err, foundInitiative) => {
-  // Initiative.findById(req.params.id, (err, foundInitiative) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('show', { initiative: foundInitiative });
-    }
-  });
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
 });
 
 
-// =======================
-// OPINION ROUTES
-// =======================
 
-// NEW OPINION
-app.get('/andelainitiative/:id/opinions/new', (req, res) => {
-  Initiative.findById(req.params.id, (err, initiative) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('opinions/new', { initiative });
-    }
-  });
-});
+// MiddleWares
+
+let isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.flash("error", "Please Login First!");
+  res.redirect("/login")
+};
 
 
-// CREATE OPINION
-app.post('/andelainitiative/:id/opinions', (req, res) => {
-  Initiative.findById(req.params.id, (err, initiative) => {
-    if (err) {
-      res.redirect('/andelainitiative');
-    } else {
-      Opinion.create(req.body.opinion, (err, opinion) => {
-        if (err) {
-          console.log(err);
-        } else {
-          initiative.opinions.push(opinion);
-          initiative.save((err, newInitiative) => {
-            console.log(newInitiative);
-          });
-          res.redirect(`/andelainitiative/${initiative._id}`);
-        }
-      });
-    }
-  });
-});
 
-// EDIT OPINION
-app.get('/andelainitiative/:id/opinions/:opinion_id/edit', (req, res) => {
-  Opinion.findById(req.params.opinion_id, (err, foundOpinion) => {
-    if (err) {
-      res.redirect('back');
-    } else {
-      res.render('opinions/edit', { initiative_id: req.params.id, opinion: foundOpinion });
-    }
-  });
-});
 
-// UPDATE OPINION AFTER EDIT
-app.put('/andelainitiative/:id/opinions/:opinion_id', (req, res) => {
-  Opinion.findByIdAndUpdate(req.params.opinion_id, req.body.opinion, (err, updatedOpinion) => {
-    if (err) {
-      res.redirect('back');
-    } else {
-      res.redirect(`/andelainitiative/${req.params.id}`);
-    }
-  });
-});
 
-// DELETE OPINION
-app.delete('/andelainitiative/:id/opinions/:opinion_id', (req, res) => {
-  Opinion.findByIdAndRemove(req.params.opinion_id, (err) => {
-    if (err) {
-      res.redirect('back');
-    } else {
-      res.redirect(`/andelainitiative/${req.params.id}`);
-    }
-  });
-});
+
+
+
+
 
 
 
