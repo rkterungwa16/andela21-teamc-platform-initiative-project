8027df9f9f063662a7e3ce47698b8ba1d0e8e03b
Ayodele Victor Muyiwa <ayodelevm@outlook.com>
2017-05-17 23:52:50 +0100

    (chore) adding css files

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 /*
  *  Like an idea post
-*/ 
+*/ /*
 let vote = (event) => {
   event.preventDefault();
   var id = $(this).attr('rel');
@@ -28,3 +28,59 @@ $(function () {
   // Click to like an idea
   $('.voteClassElement').click(vote);
 })
+*/
+
+
+// const anchoring = (event) => {
+//   event.preventDefault();
+//   var id = $( this ).attr('class');
+//   alert(id)
+//   console.log(id);
+
+//   $.ajax({
+//     type: 'GET',
+//     url: '/andelainitiative/:id/opinions/:opinion_id/upvote',
+//     data: { id: id },
+//     success: (res) => {
+//       console.log('success -->', res);
+//       $('#upvoted').text(res.upvotes.length);
+
+//     }
+//     // error: () => {
+//     //   alert('error loading')
+//     // }
+//   });
+
+// };
+// $(function () { 
+//   // Click to like an idea
+//   $('div#selectors.col-md-12').on('click', 'a', anchoring);
+// });
+
+$(function () { 
+  // Click to like an idea
+$(document).on('click', '.allVotes', (e) => {
+  e.preventDefault();
+  const button = e.target.id
+  alert(button)
+  const id = button.split('-')[1]
+  alert(id)
+  console.log(event.target.nodeName);
+  console.log(id);
+
+  $.ajax({
+    type: 'GET',
+    url: '/andelainitiative/:id/opinions/:opinion_id/upvote',
+    data: { id: id },
+    success: (res) => {
+      console.log('success -->', res);
+      $('#upvoted').text(res.upvotes.length);
+
+    }
+    // error: () => {
+    //   alert('error loading')
+    // }
+  });
+});
+});
+
