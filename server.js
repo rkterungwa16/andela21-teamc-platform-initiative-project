cffb7a87fa7b01ea709abeb66435fd89db1cd485
Mcdavid Emereuwa <mcdavidemereuwa95@gmail.com>
2017-05-17 16:04:31 +0100

    merge

--- server.js
+++ server.js
@@ -10,10 +10,18 @@ mongoose.connect('mongodb://localhost/andelainitiative');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
-// MONGOOSE/MODEL CONFIG
+// USER MONGOOSE CONFIG
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+const Users = mongoose.model('Users', UserSchema);
+
+// INITIATIVES MONGOOSE/MODEL CONFIG
 const initiativeSchema = new mongoose.Schema({
+  Author: String,
   title: String,
   image: { type: String, default: 'http://leadersinheels.com/wp-content/uploads/facebook-default.jpg' },
   body: String,
@@ -23,7 +31,7 @@ const Initiatives = mongoose.model('Initiatives', initiativeSchema);
 
 // INDEX ROUTES
 app.get('/', (req, res) => {
-  res.redirect('/andelainitiative');
+  res.redirect('/andelainitiative/login');
 });
 app.get('/andelainitiative', (req, res) => {
   Initiatives.find({}, (err, andelainitiative) => {
@@ -34,6 +42,31 @@ app.get('/andelainitiative', (req, res) => {
     }
   });
 });
+
+// User Login Page
+app.get('/andelainitiative/login', (req, res) => {
+  Users.create(req.body.user, (err, foundUsers) => {
+    if (err) {
+      res.render('login');
+    } else {
+      res.render('login', { user: foundUsers });
+    }
+  });
+});
+
+// User Dashboard page ROUTE
+
+app.get('/andelainitiatives/:id', (req, res) => {
+  Users.findById(req.params.id, (err, user) => {
+    if (err) {
+      res.redirect('login');
+    } else {
+      res.render('dashboard', { user });
+    }
+  });
+});
+
+
 // New Route
 app.get('/andelainitiative/new', (req, res) => {
   res.render('new');
@@ -96,6 +129,7 @@ app.delete('/andelainitiative/:id', (req, res) => {
     }
   });
 });
+
 app.listen(process.env.PORT || 8080, process.env.IP, () => {
   console.log('server is running');
 });
