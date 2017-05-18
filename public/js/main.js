fec790bd1bf4921e84ddce42a1b05907d5947b40
Bamidele <fisiwizy@gmail.com>
2017-05-19 00:41:56 +0100

    bug fixes: fix  vote bug

--- public/js/main.js
+++ public/js/main.js
@@ -2,22 +2,20 @@ $(() => {
   // Click to like an idea
 $(document).on('click', '.allVotes', (e) => {
   e.preventDefault();
-  const button = e.target.id
-  alert(button);
-  const id = button.split('-')[1]
-  const idName = button.split('-')[0]
+  const button = e.target.id;
+  const id = button.split('-')[1];
+  const idName = button.split('-')[0];
 
   $.ajax({
     type: 'GET',
     url: '/andelainitiative/:id/opinions/:opinion_id/upvote',
     data: { id, idName },
     success: (res) => {
-      console.log('=====', JSON.stringify(res, null, 4));
-      console.log(res.newOpinion.idName);
+      const votesDisplay = e.target.parentNode.parentNode;
       if (res.idName === 'Uvote') {
-        $('.upvoted').text(res.newOpinion.upvotes.length);
+        $(votesDisplay).find('.upvoted').text(res.newOpinion.upvotes.length);
       } if (res.idName === 'Dvote') {
-        $('.downvoted').text(res.newOpinion.downvotes.length);
+        $(votesDisplay).find('.downvoted').text(res.newOpinion.downvotes.length);
       }
     }
   });
