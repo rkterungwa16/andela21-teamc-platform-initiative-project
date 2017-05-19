f68e4c747cbfc36f7662bc5617136e02693824ec
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-05-19 09:38:35 +0100

    (chore) Add travis continuous integration

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build Status](https://travis-ci.org/rkterungwa16/andela21-teamc-platform-initiative-project.svg?branch=master)](https://travis-ci.org/rkterungwa16/andela21-teamc-platform-initiative-project.svg?branch=master)
---
+++ gulpfile.babel.js
@@ -0,0 +1,62 @@
+import gulp from 'gulp';
+import jasmineNode from 'gulp-jasmine-node';
+import babel from 'gulp-babel';
+import gulpBabelIstanbul from 'gulp-babel-istanbul';
+import coveralls from 'gulp-coveralls';
+import injectModules from 'gulp-inject-modules';
+import nodemon from 'gulp-nodemon';
+
+/**
+ *gulp task for transpiling ES6 to ES5
+ */
+gulp.task('transpile', () => {
+  gulp.src(['models/**.js', 'routes/**.js', 'serverone.js'])
+  .pipe(babel({
+    presets: ['es2015']
+  }))
+  .pipe(gulp.dest('dist/'));
+});
+
+/**
+ *default gulp task that runs whenever gulp is called without specifing a task
+ */
+gulp.task('default', ['transpile', 'serverone']);
+
+/**
+ * Gulp task for running tests Specs
+ */
+gulp.task('run-test', () => {
+  gulp.src(['tests/*Spec.js'])
+  .pipe(jasmineNode({
+    timeout: 10000
+  }));
+});
+
+/**
+ * gulp task for getting coverage report on tests
+ */
+gulp.task('coverage', (cb) => {
+  gulp.src(['/API/src/vote.js'])
+    .pipe(gulpBabelIstanbul())
+    .pipe(injectModules())
+    .on('finish', () => {
+      gulp.src('tests/*.js')
+      .pipe(babel())
+      .pipe(injectModules())
+      .pipe(jasmineNode())
+      .pipe(gulpBabelIstanbul.writeReports())
+      .pipe(gulpBabelIstanbul.enforceThresholds({ thresholds: { global: 70 } }))
+      .on('end', cb);
+    });
+});
+
+/**
+ * gulp task to send coverage reports to coveralls.io
+ */
+gulp.task('coveralls', ['coverage'], () => {
+  if (!process.env.CI) {
+    return;
+  }
+  return gulp.src('./coverage/lcov.info')
+    .pipe(coveralls());
+});
